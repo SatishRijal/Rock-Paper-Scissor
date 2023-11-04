a6ce24aa1e9d8d4e791e20799bc645f0de143027
Satish Rijal <75137862+satishrijal@users.noreply.github.com>
2023-11-04 09:33:22 +0545

    Update main.py

--- main.py
+++ main.py
@@ -5,38 +5,44 @@ choices = ['rock','paper','scissors']
 computer = random.choice(choices)
 name = input('Enter your name: ')
 
-player = None
-while player not in choices:
-    player = input('Enter rock, paper or scissors: ').lower()
+while True:
+    player = None
+    while player not in choices:
+         player = input('Enter rock, paper or scissors: ').lower()
 
-if player == computer:
-    print("Computer : "+computer)
-    print(f"{name} : "+player)
-    print('Tie!!!!')
-elif player == "rock":
-    if computer =="paper":
-        print("Computer : " + computer)
-        print(f"{name} : " + player)
-        print(f"{name} loose!!!")
-    else :
-        print("Computer : " + computer)
-        print(f"{name} : " + player)
-        print(f"{name} win!!!")
-elif player == "paper":
-    if computer == "rock":
-        print("Computer : " + computer)
-        print(f"{name} : " + player)
-        print(f"{name} win!!!")
-    else:
-        print("Computer : " + computer)
-        print(f"{name} : " + player)
-        print(f"{name} loose!!!")
-elif player == "scissors":
-    if computer == "rock":
-        print("Computer : " + computer)
-        print(f"{name} : " + player)
-        print(f"{name} loose!!!")
-    else:
-        print("Computer : " + computer)
-        print(f"{name} : " + player)
-        print(f"{name} win!!!")
+    if player == computer:
+        print("Computer : "+computer)                                              
+        print(f"{name} : "+player)
+        print('Tie!!!!')
+    elif player == "rock":
+        if computer =="paper":
+            print("Computer : " + computer)
+            print(f"{name} : " + player)
+            print(f"{name} loose!!!")
+        else :
+            print("Computer : " + computer)
+            print(f"{name} : " + player)
+            print(f"{name} win!!!")
+    elif player == "paper":
+        if computer == "rock":
+            print("Computer : " + computer)
+            print(f"{name} : " + player)
+            print(f"{name} win!!!")
+        else:
+            print("Computer : " + computer)
+            print(f"{name} : " + player)
+            print(f"{name} loose!!!")
+    elif player == "scissors":
+        if computer == "rock":
+            print("Computer : " + computer)
+            print(f"{name} : " + player)
+            print(f"{name} loose!!!")
+        else:
+            print("Computer : " + computer)
+            print(f"{name} : " + player)
+            print(f"{name} win!!!")
+    check = input("Do you want to continue (Y/N)").lower()
+    if check != "y":
+        break
+    
+print("Bye!!!")
